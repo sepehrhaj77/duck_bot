237622cf9d0c03d3b6fce55f1972d40227b85cef
Sepehr Hajbarat <sepehrhaj77@gmail.com>
2021-08-10 17:14:31 -0400

    telwksjteljistej;s

--- commands/dadJoke.js
+++ commands/dadJoke.js
@@ -1,7 +1,11 @@
 const axios = require('axios')
 
 exports.dadJoke = async () => {
-	const config = { headers: { Accept: 'application/json' } }
-	const res = await axios.get('https://icanhazdadjoke.com/', config)
-	return res.data.joke
+	try {
+		const config = { headers: { Accept: 'application/json' } }
+		const res = await axios.get('https://icanhazdadjoke.com/', config)
+		return res.data.joke
+	} catch (e) {
+		return 'no jokes. sorry'
+	}
 }
