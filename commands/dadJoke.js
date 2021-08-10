a427b537d06a17adf3d90f85afdac4ec7aea78ab
Sepehr Hajbarat <sepehrhaj77@gmail.com>
2021-08-10 17:04:46 -0400

    testing

--- commands/dadJoke.js
+++ commands/dadJoke.js
@@ -1,15 +1,13 @@
 const axios = require('axios')
 
-const getJoke = async () => {
-	try {
-		const config = { headers: { Accept: 'application/json' } }
-		const res = await axios.get('https://icanhazdadjoke.com/', config)
-		console.log(res.data.joke)
-	} catch (e) {
-		console.log(e)
-	}
-}
-
 exports.dadJoke = () => {
-	getJoke()
+	const config = { headers: { Accept: 'application/json' } }
+	console.log(
+		axios
+			.get('https://icanhazdadjoke.com/', config)
+			.then((data) => {
+				console.log(data.data.joke)
+			})
+			.catch((e) => console.log(e))
+	)
 }
