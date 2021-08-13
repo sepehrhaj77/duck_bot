b7efe00e9c0d488adc611914f741a87124f3b812
Sepehr Hajbarat <sepehrhaj77@gmail.com>
2021-08-13 12:54:45 -0400

    testin

--- commands/dadJoke.js
+++ commands/dadJoke.js
@@ -1,7 +1,10 @@
 let dadJoke = require('give-me-a-joke')
 
 exports.dadJoke = () => {
+	console.log('Starting dadjoke()...')
 	dadJoke.getRandomDadJoke(function (joke) {
-		return joke
+		console.log('calling joke()...')
+		console.log(joke())
+		return joke()
 	})
 }
