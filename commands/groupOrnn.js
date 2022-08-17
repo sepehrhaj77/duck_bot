e4a6b268ad53fd6b58a03bceb1dcde82524910d4
Sepehr Hajbarat <sepehrhaj77@gmail.com>
2022-08-17 16:38:37 -0400

    idk?

--- commands/groupOrnn.js
+++ commands/groupOrnn.js
@@ -4,7 +4,7 @@ exports.groupOrnn = async message => {
 		const connection = await message.member.voice.channel.join()
 		const dispatcher = connection.play('./GroupOrnn.mp3')
 		dispatcher.on('start', () => {
-			dispatcher.setVolume(0.7)
+			dispatcher.setVolume(0.2)
 		})
 		dispatcher.on('finish', () => {
 			connection.disconnect()
